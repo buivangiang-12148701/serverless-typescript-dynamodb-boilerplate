ee88431f1ae138ef45d44294e6c5ccc78d40dba6
pham minh hai au <aupmh@vitalify.asia>
2023-04-25 21:05:53 +0700

    test: ensure AccessToken expires in 30 minutes

--- src/template/domain/models/access-token.ts
+++ src/template/domain/models/access-token.ts
@@ -1,3 +1,11 @@
-export type AccessToken = {
-  accessToken: string
+export class AccessToken {
+  constructor (private readonly value: string) {}
+
+  getValue (): string {
+    return this.value
+  }
+
+  static get expirationInMs (): number {
+    return 30 * 60 * 1000 // 30 minutes
+  }
 }
