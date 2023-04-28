1cb2700ae881f017df67ef39215ab9503f75be95
pham minh hai au <aupmh@vitalify.asia>
2023-04-28 21:48:11 +0700

    rename folder

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { Connection } from '@/infra/db/dynamoose/helpers'
-import { makeCreateTodoHandler } from '@/main/factories/handlers'
+import { makeCreateTodoHandler } from '@/main/factories/controllers'
 
 Connection.getInstances().connect()
 export const createTodoHandler = makeCreateTodoHandler().handler
