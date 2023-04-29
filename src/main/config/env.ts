d08a8b6154fed7d567721f9b7bb2dd6f8e0a5fb9
pham minh hai au <aupmh@vitalify.asia>
2023-04-29 14:16:44 +0700

    add default for environment

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,9 @@
 import { cleanEnv, str, num, bool } from 'envalid'
 export const Env = cleanEnv(process.env, {
-  IS_OFFLINE: bool() ?? false,
-  REGION: str(),
-  DYNAMODB_TABLE_TODO: str(),
-  DYNAMODB_PROTOCOL: str() ?? 'http',
-  DYNAMODB_HOST: str() ?? 'localhost',
-  DYNAMODB_PORT: num() ?? 8000
+  IS_OFFLINE: bool({ default: false }),
+  REGION: str({ default: 'us-east-1' }),
+  DYNAMODB_TABLE_TODO: str({ default: 'todos' }),
+  DYNAMODB_PROTOCOL: str({ default: 'http' }),
+  DYNAMODB_HOST: str({ default: 'localhost' }),
+  DYNAMODB_PORT: num({ default: 8000 })
 })
