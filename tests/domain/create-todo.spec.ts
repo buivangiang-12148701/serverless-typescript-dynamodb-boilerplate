4c09d77d22c2f60d4a4339a25930f0a2cd53dcd6
pham minh hai au <aupmh@vitalify.asia>
2023-04-29 15:50:36 +0700

    test: optimize code

--- tests/domain/create-todo.spec.ts
+++ tests/domain/create-todo.spec.ts
@@ -1,14 +1,20 @@
 import { type CreateTodo } from '@/domain/usecases'
-import { mock } from 'jest-mock-extended'
+import { mock, type MockProxy } from 'jest-mock-extended'
 import { faker } from '@faker-js/faker'
 
 describe('CreateTodo', () => {
-  it('should call method `add` with correct parameters', async () => {
-    const sut = mock<CreateTodo>()
-    const params = {
+  let sut: MockProxy<CreateTodo>
+  let params: CreateTodo.Params
+  beforeAll(() => {
+    params = {
       title: faker.lorem.words(3),
       description: faker.lorem.words(10)
     }
+  })
+  beforeEach(() => {
+    sut = mock()
+  })
+  it('should call method `add` with correct parameters', async () => {
     await sut.add(params)
     expect(sut.add).toHaveBeenCalledWith(params)
     expect(sut.add).toHaveBeenCalledTimes(1)
