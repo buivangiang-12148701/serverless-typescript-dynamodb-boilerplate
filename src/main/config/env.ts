214e450ed343b3f059ebfb4f61869f62e2da88d9
pham minh hai au <aupmh@vitalify.asia>
2023-04-29 13:57:31 +0700

    update default env for dev

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,9 @@
 import { cleanEnv, str, num, bool } from 'envalid'
 export const Env = cleanEnv(process.env, {
-  IS_OFFLINE: bool(),
+  IS_OFFLINE: bool() ?? false,
   REGION: str(),
   DYNAMODB_TABLE_TODO: str(),
-  DYNAMODB_PROTOCOL: str(),
-  DYNAMODB_HOST: str(),
-  DYNAMODB_PORT: num()
+  DYNAMODB_PROTOCOL: str() ?? 'http',
+  DYNAMODB_HOST: str() ?? 'localhost',
+  DYNAMODB_PORT: num() ?? 8000
 })
