ca2f5dcc5da8d07582c0859a70e77001af75e7ae
pham minh hai au <aupmh@vitalify.asia>
2023-05-01 17:44:46 +0700

    test: test create-todo-schema.ts

--- tests/presentation/helpers/util.spec.ts
+++ tests/presentation/helpers/util.spec.ts
@@ -11,22 +11,22 @@ describe('util helper', () => {
       }
     }
 
-    test('returns the correct value for a given path', () => {
+    it('returns the correct value for a given path', () => {
       const result = get(obj, 'a.b.c')
       expect(result).toBe(1)
     })
 
-    test('returns the default value when the path is not found', () => {
+    it('returns the default value when the path is not found', () => {
       const result = get(obj, 'a.b.e', 'default')
       expect(result).toBe('default')
     })
 
-    test('handles arrays in the object', () => {
+    it('handles arrays in the object', () => {
       const result = get(obj, 'a.d[1]')
       expect(result).toBe(2)
     })
 
-    test('handles nested objects', () => {
+    it('handles nested objects', () => {
       const result = get(obj, 'a.b')
       expect(result).toEqual({ c: 1 })
     })
