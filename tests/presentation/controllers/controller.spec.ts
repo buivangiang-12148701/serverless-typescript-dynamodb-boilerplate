bf0c9151fa492d306950c518cb9b0c98b97d0e6b
pham minh hai au <aupmh@vitalify.asia>
2023-05-01 19:32:53 +0700

    simplify code

--- tests/presentation/controllers/controller.spec.ts
+++ tests/presentation/controllers/controller.spec.ts
@@ -1,6 +1,10 @@
 import { type APIGatewayProxyEvent } from 'aws-lambda'
 import { Controller } from '@/presentation/controllers'
+import type middy from '@middy/core'
+import { MiddlewareBuilder } from '@/presentation/middlewares'
+import { faker } from '@faker-js/faker'
 
+jest.spyOn(console, 'log').mockImplementation(() => {})
 class TestController extends Controller {
   async perform (_event: APIGatewayProxyEvent) {
     return {
@@ -10,12 +14,80 @@ class TestController extends Controller {
   }
 }
 
+class TestControllerWithMiddleware extends Controller {
+  override get middlewares (): Array<middy.MiddlewareObj<any, any, Error, any>> {
+    return [
+      ...MiddlewareBuilder.of()
+        .withJsonBodyParser()
+        .withSecurityHeaders().build()
+    ]
+  }
+
+  async perform (_event: APIGatewayProxyEvent) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Hello from TestControllerWithMiddleware!' })
+    }
+  }
+}
+
+class TestControllerOverrideHook extends Controller {
+  override async perform (_event: APIGatewayProxyEvent) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Hello from TestControllerOverrideHook!' })
+    }
+  }
+
+  override async beforePrefetch (): Promise<void> {
+    console.log('beforePrefetch')
+    return Promise.resolve()
+  }
+
+  override async requestStart (): Promise<void> {
+    console.log('requestStart')
+    return Promise.resolve()
+  }
+
+  override async beforeMiddleware (_fctName: string): Promise<void> {
+    console.log('beforeMiddleware')
+    return Promise.resolve()
+  }
+
+  override async afterMiddleware (_fctName: string): Promise<void> {
+    console.log('afterMiddleware')
+    return Promise.resolve()
+  }
+
+  override async beforeHandler (): Promise<void> {
+    console.log('beforeHandler')
+    return Promise.resolve()
+  }
+
+  override async afterHandler (): Promise<void> {
+    console.log('afterHandler')
+    return Promise.resolve()
+  }
+
+  override async requestEnd (): Promise<void> {
+    console.log('requestEnd')
+    return Promise.resolve()
+  }
+}
+
 describe('Controller', () => {
   let controller: TestController
+  let controllerWithMiddleware: TestControllerWithMiddleware
+  let controllerOverrideHook: TestControllerOverrideHook
+  let logSpy: jest.SpyInstance
   let event: APIGatewayProxyEvent
 
   beforeEach(() => {
     controller = new TestController()
+    controllerWithMiddleware = new TestControllerWithMiddleware()
+    controllerOverrideHook = new TestControllerOverrideHook()
+    logSpy = jest.spyOn(console, 'log')
+
     event = {
       body: JSON.stringify({ key: 'value' }),
       headers: {},
@@ -61,13 +133,57 @@ describe('Controller', () => {
     }
   })
 
-  test('should return a response with statusCode 200 and a message', async () => {
+  it('should return a response with statusCode 200 and a message', async () => {
     const response = await controller.perform(event)
     expect(response.statusCode).toEqual(200)
     expect(JSON.parse(response.body)).toEqual({ message: 'Hello from TestController!' })
   })
 
-  test('should have no middlewares', () => {
+  it('should have no middlewares', () => {
     expect(controller.middlewares.length).toEqual(0)
+  })
+
+  it('should have 2 middlewares', () => {
+    expect(controllerWithMiddleware.middlewares.length).toEqual(2)
+  })
+
+  it('should override beforePrefetch method', async () => {
+    await controllerOverrideHook.beforePrefetch()
+    expect(logSpy).toHaveBeenCalledWith('beforePrefetch')
+    expect.assertions(1)
+  })
+  it('should override requestStart method', async () => {
+    await controllerOverrideHook.requestStart()
+    expect(logSpy).toHaveBeenCalledWith('requestStart')
+    expect.assertions(1)
+  })
+  it('should override beforeMiddleware method', async () => {
+    await controllerOverrideHook.beforeMiddleware(faker.lorem.words(5))
+    expect(logSpy).toHaveBeenCalledWith('beforeMiddleware')
+    expect.assertions(1)
+  })
+
+  it('should override afterMiddleware method', async () => {
+    await controllerOverrideHook.afterMiddleware(faker.lorem.words(5))
+    expect(logSpy).toHaveBeenCalledWith('afterMiddleware')
+    expect.assertions(1)
+  })
+
+  it('should override beforeHandler method', async () => {
+    await controllerOverrideHook.beforeHandler()
+    expect(logSpy).toHaveBeenCalledWith('beforeHandler')
+    expect.assertions(1)
+  })
+
+  it('should override afterHandler method', async () => {
+    await controllerOverrideHook.afterHandler()
+    expect(logSpy).toHaveBeenCalledWith('afterHandler')
+    expect.assertions(1)
+  })
+
+  it('should override requestEnd method', async () => {
+    await controllerOverrideHook.requestEnd()
+    expect(logSpy).toHaveBeenCalledWith('requestEnd')
+    expect.assertions(1)
   })
 })
--- tests/presentation/middlewares/print-middleware.spec.ts
+++ tests/presentation/middlewares/print-middleware.spec.ts
@@ -10,34 +10,32 @@ const createRequest = (event: any = {}, context: any = {}): any => ({
 })
 
 describe('PrintMiddleware', () => {
-  it('PrintMiddleware: before logs the correct message and returns a resolved promise', async () => {
-    const middleware = new PrintMiddleware()
-    const request = createRequest()
-    const logSpy = jest.spyOn(console, 'log')
+  let logSpy: jest.SpyInstance
+  let request: any
+  let sut: PrintMiddleware
 
-    await middleware.before(request)
+  beforeEach(() => {
+    request = createRequest()
+    logSpy = jest.spyOn(console, 'log')
+    sut = new PrintMiddleware()
+  })
+
+  it('PrintMiddleware: before logs the correct message and returns a resolved promise', async () => {
+    await sut.before(request)
 
     expect(logSpy).toHaveBeenCalledWith('PrintMiddleware: before ', request)
     expect.assertions(1)
   })
 
   it('PrintMiddleware: after logs the correct message and returns a resolved promise', async () => {
-    const middleware = new PrintMiddleware()
-    const request = createRequest()
-    const logSpy = jest.spyOn(console, 'log')
-
-    await middleware.after(request)
+    await sut.after(request)
 
     expect(logSpy).toHaveBeenCalledWith('PrintMiddleware: after ', request)
     expect.assertions(1)
   })
 
   test('PrintMiddleware: onError logs the correct message and returns a resolved promise', async () => {
-    const middleware = new PrintMiddleware()
-    const request = createRequest()
-    const logSpy = jest.spyOn(console, 'log')
-
-    await middleware.onError(request)
+    await sut.onError(request)
 
     expect(logSpy).toHaveBeenCalledWith('PrintMiddleware: onError ', request)
     expect.assertions(1)
