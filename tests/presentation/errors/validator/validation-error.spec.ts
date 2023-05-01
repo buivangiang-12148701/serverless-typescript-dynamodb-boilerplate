d1d9cda64fca2fa41f2b4fe05f444946968ed47a
pham minh hai au <aupmh@vitalify.asia>
2023-05-01 17:30:20 +0700

    test: test validation-error.ts

---
+++ tests/presentation/errors/validator/validation-error.spec.ts
@@ -0,0 +1,38 @@
+import { type BuiltInMessages } from 'fastest-validator'
+import { FastestValidatorError } from '@/presentation/errors'
+
+describe('FastestValidatorError', () => {
+  // Test constructor
+  it('should create an instance of FastestValidatorError with the given parameters', () => {
+    const params = {
+      field: 'username',
+      type: 'required' as keyof BuiltInMessages,
+      message: 'The "username" field is required.'
+    }
+
+    const error = new FastestValidatorError(params)
+
+    expect(error).toBeInstanceOf(FastestValidatorError)
+    expect(error.field).toEqual(params.field)
+    expect(error.type).toEqual(params.type)
+    expect(error.message).toEqual(params.message)
+  })
+
+  // Test toJSON method
+  it('should return a JSON representation of the error', () => {
+    const params = {
+      field: 'username',
+      type: 'required' as keyof BuiltInMessages,
+      message: 'The "username" field is required.'
+    }
+
+    const error = new FastestValidatorError(params)
+    const json = error.toJSON()
+
+    expect(json).toEqual({
+      field: params.field,
+      type: params.type,
+      message: params.message
+    })
+  })
+})
